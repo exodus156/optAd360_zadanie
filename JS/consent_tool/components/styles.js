0c4693f8a6039a68f2f7ca880c8ce545d85012cd
Michal Dzialek <exodusofdead@gmail.com>
2021-03-31 16:48:02 +0200

    Finished application

--- JS/consent_tool/components/styles.js
+++ JS/consent_tool/components/styles.js
@@ -19,19 +19,21 @@ const styles = `
 .consent-wrapper-content {
     display: inline-block;
     width: 500px;
-    height: 600px;
-    background-color: rgb(255, 255, 255);
+    max-height: 700px;
+    background-color: #fcfcfc;
     box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
     
 }
 
 .consent-content {
-    margin: 20px 30px;  
+    margin: 20px 30px;
+    background-color: #fcfcfc;    
 }
 
 .consent-title {
     padding-top: 10px;
     text-align: center;
+    background-color: #fcfcfc;
 }
 
 .consent-list {
@@ -39,6 +41,7 @@ const styles = `
     overflow-y: scroll;
     padding-left: 20px;
     padding-right: 20px;
+    background-color: rgb(255, 255, 255);
     box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px, rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
 }
 
@@ -47,6 +50,36 @@ const styles = `
     display: flex;
     align-items: center;
     justify-content: space-around;
+    background-color: #fcfcfc;
+}
+
+.consent-grid-container {
+    display: grid;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    grid-template-columns: 150px 130px auto;
+    text-align: center;
+    grid-column-gap: 5px;
+    align-items: center;
+    border-bottom: 1.5px solid black;
+    min-height: 30px;
+}
+
+.consent-grid-container-sticky {
+    padding-top: 12px;
+    position: sticky;
+    top: 0;
+    background-color: rgb(255, 255, 255);
+}
+.consent-list-checkbox {
+    display: grid;
+    grid-template-columns: auto 80px;
+    align-items: center;
+}
+#consent-error {
+    display: none;
+    color: red;
+    text-align: center;
 }
 `
 
--- JS/consent_tool/tool.js
+++ JS/consent_tool/tool.js
@@ -1,30 +1,88 @@
 import insertCss from './functions/addCSS.js'
 import styles from './components/styles.js'
 import fetchData from './functions/fetchData.js'
+import selectAllCheckboxes from './functions/selectAllCheckboxes.js'
 
-const data = await fetchData('https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json');
-const vendors = Object.values(data.vendors);
-console.log(vendors);
+if(document.cookie.length < 14) {
+    const data = await fetchData('https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json');
+    const vendors = Object.values(data.vendors);
 
-document.body.className = 'consent';
-document.body.innerHTML += `
-    <div class="consent-wrapper-overlay">
-        <div class="consent-wrapper-content">
-            <div class="consent-content">
-                <h1 class="consent-title">GDPR consent</h1>
-                <form class="consent-form">
-                    <ul class="consent-list">
-                        
-                    </ul>
-                    <div class="consent-buttons">
-                        <button id="accept" type="submit">Accept</button>
-                        <button id="reject">Reject</button>
-                    </div>
-                </form>
+    document.body.className = 'consent';
+    document.body.insertAdjacentHTML('afterbegin', `
+        <div class="consent-wrapper-overlay" id="consent-widget">
+            <div class="consent-wrapper-content">
+                <div class="consent-content">
+                    <h1 class="consent-title">GDPR consent</h1>
+                    <form id="consent-form">
+                        <ul class="consent-list">
+                            <li class="consent-grid-container consent-grid-container-sticky">
+                                <div class="consent-list-name">Nazwa partnera</div>
+                                <div class="consent-list-url">Link do polityki prywatności</div>
+                                <div class="consent-list-checkbox">
+                                    <input type="checkbox" id="select-all"/>
+                                    <label for="select-all">Zaznacz wszystkie</label>
+                                </div>
+                            </li>
+                            ${vendors.map((vendor) => {
+                                return `<li class="consent-grid-container">
+                                    <div class="consent-list-name">${vendor.name}</div>
+                                    <div class="consent-list-url"><a href=${vendor.policyUrl}>Polityka prywatności</a></div>
+                                    <div class="consent-list-checkbox">
+                                        <input type="checkbox" id=${vendor.id} />
+                                        <label for=${vendor.id}>Zaznacz</label>
+                                    </div>
+                                </li>`
+                            }).join('')}
+                        </ul>
+                        <div id="consent-error">
+                            <h2>Proszę wybrać conajmniej jednego partnera.</h2>
+                        </div>
+                        <div class="consent-buttons">
+                            <button id="accept" type="submit">Accept</button>
+                            <input type="button" onclick="location.href='https://google.com';" value="Reject" />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-`
+    `)
+    insertCss(styles);
 
-insertCss(styles);
+    document.getElementById('select-all').onclick = () => {
+        selectAllCheckboxes();
+    }
 
+    document.getElementById('consent-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        let checkedBoxes = Array.from(document.querySelectorAll('input[type=checkbox]:checked'));
+        let vendorIDs = checkedBoxes.map(box => {
+            return box.id
+        });
+        let selectAllIndex = vendorIDs.indexOf("select-all");
+        if(selectAllIndex === 0){
+            vendorIDs.splice(selectAllIndex, 1);
+        }
+        
+        //small validation
+        if(checkedBoxes.length === 0){
+
+            document.getElementById('consent-error').style.display = "block";
+
+        } else {
+            document.getElementById('consent-error').style.display = "none";
+
+            /* Create cookie expiration date */
+            let d = new Date();
+            d.setDate(d.getDate() + 1);
+            const expires = "expires=" + d.toUTCString();
+
+            /* Create list of accepted vendors */
+            const acceptedVendors = vendorIDs.toString();
+
+            document.cookie = "ConsentCookie=" + acceptedVendors + "; " + expires;
+
+            document.body.className = "";
+            document.getElementById('consent-widget').style.display = 'none';
+        }
+    })
+}
